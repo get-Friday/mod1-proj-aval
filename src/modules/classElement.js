8bd3fb927916509e70e21de6d9bbd824c95bd07f
get-Friday <gabi.get@hotmail.com>
2022-04-13 11:57:02 -0300

    class for HTML element and attributes

---
+++ src/modules/classElement.js
@@ -0,0 +1,28 @@
+// This class is responsible for creating HTML elements and its attributes
+class HTMLAttribute {
+    key
+    value
+    constructor(key, value) {
+        this.key = key
+        this.value = value
+    }
+}
+
+class HTMLElement {
+    name
+    attributeList
+    addAttribute(attribute) {
+        if (!(attribute instanceof HTMLAttribute)) {
+            return
+        }
+
+        if (this.attributeList.includes(attribute)) {
+            return
+        }
+
+        this.attributeList.push(attribute)
+    }
+    constructor(name) {
+        this.name = name
+    }
+}
